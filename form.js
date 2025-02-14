ec2a5a433c6ac5b09eb53241c180cc4474e3ab6b
GANEPAL\william.brinez <auxiliardesarrollo3@ganepalmira.com.co>
2025-02-14 15:46:27 -0500

    se cambia el tiempo de la recarga de la pagina cuando finaliza el envio del formulario

--- form.js
+++ form.js
@@ -1,55 +1,56 @@
-
 document.querySelector("#form").addEventListener("submit", function (event) {
-    event.preventDefault()
+    event.preventDefault() // Prevenir el envío del formulario por defecto
 
+    // Inicializar los toasts de Bootstrap
     let toastCargando = new bootstrap.Toast(document.getElementById('liveToast'))
     let toastOk = new bootstrap.Toast(document.getElementById('OkToast'))
     let toastError = new bootstrap.Toast(document.getElementById('errorToast'))
     let toastFoto = new bootstrap.Toast(document.getElementById('fotoToast'))
 
+    let form = event.target // Obtener el formulario
 
-    let form = event.target
-
+    // Obtener los valores de los campos del formulario
     let nomb = document.getElementById("Nombres")
     let ape = document.getElementById("Apellidos")
     let nDocumento = document.getElementById("N.Documento")
     let nomC = "Registro " + nomb.value + " " + ape.value
 
+    // Crear un nuevo input oculto para el asunto del correo
     let newInput = document.createElement("input")
     newInput.type = "hidden"
     newInput.name = "_subject"
     newInput.value = nomC
     form.appendChild(newInput)
 
-
-    let formData = new FormData(form)
+    let formData = new FormData(form) // Crear un FormData con los datos del formulario
     if (window.fileBlob) {
+        // Si hay una imagen capturada, agregarla al FormData
         formData.append("file", window.fileBlob, nDocumento.value + ".png")
-        formData.set
     } else {
+        // Mostrar el toast de foto si no hay imagen capturada
         toastFoto.show()
         return
     }
+    let btnEnviar = document.getElementById("liveToastBtn")
+    btnEnviar.disabled = true // Deshabilitar el botón de enviar
+    toastCargando.show() // Mostrar el toast de cargando
 
-    toastCargando.show()
+    // Enviar el formulario usando fetch
     fetch("https://formsubmit.co/william.brinez017@gmail.com", {
         method: "POST",
         body: formData
     }).then(response => {
+        toastCargando.hide() // Ocultar toastCargando después de recibir la respuesta
         if (response.ok) {
-            toastOk.show()
-            form.reset()
-            setTimeout(() => { location.reload() }, 1500);
+            toastOk.show() // Mostrar toast de éxito
+            form.reset() // Reiniciar el formulario
+            setTimeout(() => { location.reload() }, 1200); // Recargar la página después de 1 segundo
         } else {
-            toastError.show()
+            toastError.show() // Mostrar toast de error
         }
     }).catch(error => {
         console.log(error)
-        toastError.show()
+        toastCargando.hide() // Ocultar toastCargando en caso de error
+        toastError.show() // Mostrar toast de error
     })
-
 })
-
-
-
-
