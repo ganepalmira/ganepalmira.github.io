a7af211d01d1813c6152506947dd584908b27789
GANEPAL\william.brinez <auxiliardesarrollo3@ganepalmira.com.co>
2025-02-14 14:50:05 -0500

    Cambio en el boton de tomar la foto y se corrige error en el diseño de los mensajes

--- form.js
+++ form.js
@@ -1,15 +1,15 @@
 
-document.querySelector("#form").addEventListener("submit", function (event){
+document.querySelector("#form").addEventListener("submit", function (event) {
     event.preventDefault()
 
     let toastCargando = new bootstrap.Toast(document.getElementById('liveToast'))
     let toastOk = new bootstrap.Toast(document.getElementById('OkToast'))
     let toastError = new bootstrap.Toast(document.getElementById('errorToast'))
     let toastFoto = new bootstrap.Toast(document.getElementById('fotoToast'))
-    
+
 
     let form = event.target
-   
+
     let nomb = document.getElementById("Nombres")
     let ape = document.getElementById("Apellidos")
     let nDocumento = document.getElementById("N.Documento")
@@ -20,34 +20,34 @@ document.querySelector("#form").addEventListener("submit", function (event){
     newInput.name = "_subject"
     newInput.value = nomC
     form.appendChild(newInput)
-    
+
 
     let formData = new FormData(form)
-    if(window.fileBlob){
+    if (window.fileBlob) {
         formData.append("file", window.fileBlob, nDocumento.value + ".png")
         formData.set
-    }else{
+    } else {
         toastFoto.show()
         return
     }
 
     toastCargando.show()
-    fetch("https://formsubmit.co/william.brinez017@gmail.com",{
+    fetch("https://formsubmit.co/william.brinez017@gmail.com", {
         method: "POST",
-        body: formData 
-    }).then(response =>{
-        if(response.ok){
-            toastCargando.hide()
+        body: formData
+    }).then(response => {
+        if (response.ok) {
             toastOk.show()
             form.reset()
-        }else{
+            setTimeout(() => { location.reload() }, 1500);
+        } else {
             toastError.show()
         }
-    }).catch(error =>{
+    }).catch(error => {
         console.log(error)
         toastError.show()
     })
-    
+
 })
 
 
